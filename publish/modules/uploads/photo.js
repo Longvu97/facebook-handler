256d792f43c3e03d6ba605e84fd507f8dbc10833
Long Vu <longvu@MacBook-Pro-cua-Long.local>
2025-12-16 14:18:39 +0700

    Add upload video
    - Add upload video
    - Refactor publish post and photo

--- publish/modules/uploads/photo.js
+++ publish/modules/uploads/photo.js
@@ -1,34 +1,37 @@
 const axiosClient = require('../../clients/axios');
-const { resolveContentType, decodeBase64File } = require('../../utils');
 
 const { FACEBOOK_URL } = require('../../setting');
 
-async function upload(platformId, accessToken, photo) {
-  const { base64, type } = photo;
-
-  const bodyFormData = new FormData();
-  const contentType = resolveContentType(base64, type);
-  const buffer = decodeBase64File(base64);
-  const extension = contentType.split('/')[1] || 'bin';
-
-  bodyFormData.append(
-    'source',
-    new Blob([buffer], { type: contentType }),
-    `upload.${extension}`,
-  );
-  bodyFormData.append('access_token', accessToken);
-  bodyFormData.append('published', 'false');
+/**
+ *
+ * @param {string} platformId
+ * @param {string} accessToken
+ * @param {object} config
+ * @param {string} config.assetUrl
+ * @param {number} config.index
+ * @returns
+ */
+async function upload(platformId, accessToken, config) {
+  console.log('Publish photo', 'config', config);
+  const { assetUrl, index } = config;
 
   try {
-    const response = await axiosClient({
-      method: 'post',
-      url: `${FACEBOOK_URL}/${platformId}/photos`,
-      data: bodyFormData,
-      headers: bodyFormData.getHeaders ? bodyFormData.getHeaders() : {},
-      maxBodyLength: Infinity,
-    });
+    const response = await axiosClient.post(
+      `${FACEBOOK_URL}/${platformId}/photos`,
+      {
+        url: assetUrl,
+        published: false,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
 
-    return { id: response.data.id, index: photo.index };
+    console.log('Publish photo', 'photo response', response.data);
+
+    return { id: response.data.id, index };
   } catch (error) {
     console.error('Facebook photo upload failed', error);
     throw error;
