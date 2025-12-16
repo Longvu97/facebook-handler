272cd6acd28a45b5d84993995fbf71a3ba60cb1b
Long Vu <longvu@MacBook-Pro-cua-Long.local>
2025-12-16 16:56:16 +0700

    Add lodash sort by

--- publish/index.js
+++ publish/index.js
@@ -1,8 +1,8 @@
-const AWSXRay = require('aws-xray-sdk');
+// const AWSXRay = require('aws-xray-sdk');
 
-AWSXRay.captureAWS(require('aws-sdk'));
-AWSXRay.captureHTTPsGlobal(require('http'));
-AWSXRay.captureHTTPsGlobal(require('https'));
+// AWSXRay.captureAWS(require('aws-sdk'));
+// AWSXRay.captureHTTPsGlobal(require('http'));
+// AWSXRay.captureHTTPsGlobal(require('https'));
 
 const { eventHandler, main } = require('./main');
 
