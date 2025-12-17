711a2a01e6fac4e76fc140d6e32634e4e726310e
Long Vu <longvu@MacBook-Pro-cua-Long.local>
2025-12-17 10:09:58 +0700

    Batch uploading photos

--- publish/modules/uploads/post.js
+++ publish/modules/uploads/post.js
@@ -1,5 +1,3 @@
-const sortBy = require('lodash.sortby');
-
 const uploadPhoto = require('./photo');
 const axiosClient = require('../../clients/axios');
 
@@ -19,12 +17,7 @@ async function upload(config) {
 
     let assetIds = [];
     if (urls.length) {
-      const assets = await Promise.all(
-        urls
-          .map((item) => uploadPhoto(platformId, accessToken, item)),
-      );
-
-      assetIds = sortBy(assets, ['index']).map(({ id }) => ({ media_fbid: id }));
+      assetIds = await uploadPhoto(platformId, accessToken, urls);
     }
 
     const params = {
@@ -49,22 +42,5 @@ async function upload(config) {
     throw error;
   }
 }
-
-// upload({
-//   post: {
-//     files: [{
-//       name: 'background.jpeg',
-//     }, {
-//       name: 'Share Selection - square.jpg',
-//     }],
-//     userId: '693bc8292fc8f26fdc3b9587',
-//     uploadId: '759e8f44-0e4c-4a6d-a75a-1d7d35fd9b13',
-//     description: 'This is a test upload',
-//   },
-//   connector: {
-//     platformId: '842384928966975',
-//     accessToken: 'EAALwCEqw38sBQM1NiEGBJc7wgHkzBJTWDFWlZB7oNpQ9uLXH8k80l7BIQ18mkJLT2eb8BqHvTEZADSR3C1Vy3S7nJeixCAY0ndvjkqFdvQ93QtaYi3IhSpS83KEX1xAdaJnh1sWx0rng7NQZCYgeyfKcUhbNm1eVz8YrELHPzUAEoZAb1ZBRaQiEuV9mZA08KAbQeT',
-//   },
-// });
 
 module.exports = upload;
